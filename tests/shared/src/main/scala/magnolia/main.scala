382b66f698917faae729b3055583ee7947a2469e
Jon Pretty <jon.pretty@propensive.com>
2017-06-12 14:23:06 +0200

    Slight tidyup of examples

--- tests/shared/src/main/scala/magnolia/main.scala
+++ tests/shared/src/main/scala/magnolia/main.scala
@@ -5,10 +5,9 @@ import examples._
 object Main {
   def main(args: Array[String]): Unit = {
     println(Branch(Branch(Leaf(1), Leaf(2)), Leaf(3)).show)
-
-    println(List[Entity](Person("Jon Pretty", Address(List("Home"), Country("UK", "GBR", false)))).show)
-  
+    println(List[Entity](Person("John Smith",
+        Address(List("1 High Street", "London", "SW1A 1AA"),
+        Country("UK", "GBR", false)))).show)
   }
-
 }
 
