dfb9ece2dcd0352c7953fe372d6772776d18e3e5
Jon Pretty <jon.pretty@propensive.com>
2018-06-05 17:14:14 +0100

    Revert "Change lazy val to strict val."
    This reverts commit e6c3db8a762f82f4f319f97abbbe79b5d92e2e2e.

--- core/shared/src/main/scala/magnolia.scala
+++ core/shared/src/main/scala/magnolia.scala
@@ -249,7 +249,7 @@ object Magnolia {
                   }
 
                 val ref = TermName(c.freshName("paramTypeclass"))
-                val assigned = q"""val $ref = $derivedImplicit"""
+                val assigned = q"""lazy val $ref = $derivedImplicit"""
                 CaseParam(param, repeated, assigned, paramType, ref) :: acc
               } { backRef =>
                 CaseParam(param, repeated, q"()", paramType, backRef.ref) :: acc
