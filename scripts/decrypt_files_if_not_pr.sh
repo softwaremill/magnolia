bb751bce319515765a1612b4b2eca95c31b90245
ghostbuster91 <kghost0@gmail.com>
2019-09-23 18:13:39 +0200

    Do not attempt to decrypt secrets if invoked from pull request

---
+++ scripts/decrypt_files_if_not_pr.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
+    openssl aes-256-cbc -K $encrypted_a92a7858a56e_key -iv $encrypted_a92a7858a56e_iv -in secrets.tar.enc -out secrets.tar -d
+    tar xvf secrets.tar
+fi
