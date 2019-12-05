fbbdf34de81bdca0e85484b876846e7ab4e464a4
Pedro Rodriguez <pedro@pjrt.co>
2019-12-05 10:22:47 -0500

    Fix #193: Tuple 22 issue

--- tests/src/main/scala/tests.scala
+++ tests/src/main/scala/tests.scala
@@ -141,7 +141,33 @@ object PrivateValueClass {
 }
 
 case class KArray(value: List[KArray])
-case class Wrapper(v:Option[KArray])
+case class Wrapper(v: Option[KArray])
+
+case class VeryLong(
+  p1: String,
+  p2: String,
+  p3: String,
+  p4: String,
+  p5: String,
+  p6: String,
+  p7: String,
+  p8: String,
+  p9: String,
+  p10: String,
+  p11: String,
+  p12: String,
+  p13: String,
+  p14: String,
+  p15: String,
+  p16: String,
+  p17: String,
+  p18: String,
+  p19: String,
+  p20: String,
+  p21: String,
+  p22: String,
+  p23: String
+)
 
 object Tests extends TestApp {
 
@@ -534,5 +560,33 @@ object Tests extends TestApp {
     test("equality of Wrapper") {
       Eq.gen[Wrapper].equal(Wrapper(Some(KArray(KArray(Nil) :: Nil))), Wrapper(Some(KArray(KArray(Nil) :: KArray(Nil) :: Nil))))
     }.assert(_ == false)
+
+    test("very long") {
+      val vl =
+        VeryLong("p1",
+                 "p2",
+                 "p3",
+                 "p4",
+                 "p5",
+                 "p6",
+                 "p7",
+                 "p8",
+                 "p9",
+                 "p10",
+                 "p11",
+                 "p12",
+                 "p13",
+                 "p14",
+                 "p15",
+                 "p16",
+                 "p17",
+                 "p18",
+                 "p19",
+                 "p20",
+                 "p21",
+                 "p22",
+                 "p23")
+      Eq.gen[VeryLong].equal(vl, vl)
+    }.assert(_ == true)
   }
 }
