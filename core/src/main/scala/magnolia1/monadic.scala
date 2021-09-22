5cbd09563937636b2b720c60a6d19949414a4b79
Pask <b.zylinski.o@gmail.com>
2021-09-22 11:16:19 +0200

    Rewrite instance as defs

--- core/src/main/scala/magnolia1/monadic.scala
+++ core/src/main/scala/magnolia1/monadic.scala
@@ -12,41 +12,47 @@ trait Monadic[F[_]] {
 
 object Monadic {
 
-  implicit object monadicOpt extends Monadic[Option] {
+  private object monadicOptionOps extends Monadic[Option] {
     def point[A](value: A): Option[A] = Some(value)
     def map[A, B](from: Option[A])(fn: A => B): Option[B] = from.map(fn)
     def flatMap[A, B](from: Option[A])(fn: A => Option[B]): Option[B] = from.flatMap(fn)
   }
 
-  implicit object monadicList extends Monadic[List] {
+  final implicit def monadicOption: Monadic[Option] = monadicOptionOps
+
+  private object monadicListOps extends Monadic[List] {
     def point[A](value: A): List[A] = List(value)
     def map[A, B](from: List[A])(fn: A => B): List[B] = from.map(fn)
     def flatMap[A, B](from: List[A])(fn: A => List[B]): List[B] = from.flatMap(fn)
   }
 
-  class monadicFuture(implicit ec: ExecutionContext) extends Monadic[Future] {
+  final implicit def monadicList: Monadic[List] = monadicListOps
+
+  private class monadicFutureOps(implicit ec: ExecutionContext) extends Monadic[Future] {
     def point[A](value: A): Future[A] = Future(value)
     def map[A, B](from: Future[A])(fn: A => B): Future[B] = from.map(fn)
     def flatMap[A, B](from: Future[A])(fn: A => Future[B]): Future[B] = from.flatMap(fn)
   }
 
-  final implicit def monadicFuture(implicit ec: ExecutionContext): monadicFuture = new monadicFuture()
+  final implicit def monadicFuture(implicit ec: ExecutionContext): Monadic[Future] = new monadicFutureOps()
 
-  class monadicEither[Err] extends Monadic[Either[Err, *]] {
+  private class monadicEitherOps[Err] extends Monadic[Either[Err, *]] {
     def point[A](value: A): Either[Err, A] = Right(value)
     def map[A, B](from: Either[Err, A])(fn: A => B): Either[Err, B] = from.map(fn)
     def flatMap[A, B](from: Either[Err, A])(fn: A => Either[Err, B]): Either[Err, B] = from.flatMap(fn)
   }
 
-  final implicit def monadicEither[Err]: monadicEither[Err] = new monadicEither[Err]
+  final implicit def monadicEither[T]: Monadic[Either[T, *]] = new monadicEitherOps
 
-  implicit object monadicTry extends Monadic[Try] {
+  private object monadicTryOps extends Monadic[Try] {
     def point[A](value: A): Try[A] = Success(value)
     def map[A, B](from: Try[A])(fn: A => B): Try[B] = from.map(fn)
     def flatMap[A, B](from: Try[A])(fn: A => Try[B]): Try[B] = from.flatMap(fn)
   }
 
-  final implicit class Ops[F[_], A](val fv:F[A]) extends AnyVal {
+  final implicit def monadicTry: Monadic[Try] = monadicTryOps
+
+  final implicit class Ops[F[_], A](val fv: F[A]) extends AnyVal {
     def flatMap[B](f: A => F[B])(implicit monadic: Monadic[F]): F[B] = monadic.flatMap(fv)(f)
     def map[B](f: A => B)(implicit monadic: Monadic[F]): F[B] = monadic.map(fv)(f)
   }
